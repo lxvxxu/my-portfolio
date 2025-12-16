8fde6a9dafefe6f02223e8afeccff7809caddcc5
lxvxxu <lxvxxu@gmail.com>
2025-12-17 01:38:15 +0900

    fix: I don't know

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'), // require 구문을 사용하여 모듈을 명시적으로 로드
-    require('autoprefixer'), // require 구문을 사용하여 모듈을 명시적으로 로드
-  ],
-};
+  plugins: {
+    '@tailwindcss/postcss': {}, // <-- 'tailwindcss' 대신 '@tailwindcss/postcss'로 변경
+    'autoprefixer': {},
+  },
+}
